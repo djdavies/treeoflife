f843a8669de73660caab816dbd6215e4e324578c
Aled Davies <aleddavies123@yahoo.co.uk>
2014-11-01 20:16:13 +0000

    change links_tables name to taxa

--- app/models/classification/Taxon.php
+++ app/models/classification/Taxon.php
@@ -6,63 +6,32 @@
      */
     class Taxon extends Eloquent{
         public $timestamps = false;
+        public $table = 'taxa';
 
         public function taxonomy(){
             $this->hasMany('Taxonomy');
         }
 
         public function getChildren($parentId = 0){
-            $children = $this::where('parent_id', $parentId )->get()->toArray();
+            $children = $this::where('taxa.parent_id', $parentId )
+                ->join('taxonomies', function($join)
+                {
+                    $join->on('taxa.taxa_name', '=', 'taxonomies.id');
+                })
+                ->get()->toArray();
             return $children;
         }
 
-        public function getTree($parent_id, $increment = 0){
-            $myArray = $this->getChildren($parent_id, $increment);
-            if($increment == 30) {
-                return;
-            }else {
-                if (count($myArray) == 0) {
-                    echo '</div>';
-                } else if (count($myArray) == 1) {
-                    echo "<div class='branch " . $myArray[0]['taxonomic_rank'] . " hidden'>";
-                    foreach ($myArray as $key => $value) {
-                        if ($value['parent_id'] != $parent_id) continue;
-                        echo "<div class='entry sole'>
-                                 <span class='label'>
-                                    <i class='glyphicon glyphicon-chevron-left pull-left expand-tree'></i>
-                                        ".$value['name'] ."
-                                    <i class='glyphicon glyphicon-chevron-right pull-right expand-tree'></i>
-                                 </span>";
-                        $increment++;
-                        $this->getTree($value['id'], $increment);
-                    }
-                    echo '</div></div>';
-                } else {
-                    echo "<div class='branch " . $myArray[0]['taxonomic_rank'] . " hidden'>";
-                    foreach ($myArray as $key => $value) {
-                        if ($value['parent_id'] != $parent_id) continue;
-                        echo "<div class='entry'>
-                             <span class='label'>
-                                <i class='glyphicon glyphicon-chevron-left pull-left expand-tree'></i>
-                                    ".$value['name'] ."
-                                <i class='glyphicon glyphicon-chevron-right pull-right expand-tree'></i>
-                             </span>";
-                        $increment++;
-                        $this->getTree($value['id'], $increment);
-                    }
-                    echo '</div></div>';
-                }
-            }
-        }
+
 
         public function showChildren($parent_id){
             $class = Taxonomy::all();
-            $children = $this::where('parent_id', $parent_id )->get()->toArray();
+            $children = $this->getChildren($parent_id);
             if(count($children) == 1){
-                echo "<div class='branch " . $children[0]['taxonomic_rank'] . " hidden'>";
+                echo "<div class='branch " . $children[0]['taxa_name'] . " hidden'>";
                 foreach ($children as $key => $value) {
                     echo "<div class='entry sole'>
-                     <span class='label' data-classification='$value->level' data-id='$value->id'>
+                     <span class='label' data-classification='$value[taxa_name]' data-id='$value[id]'>
                         <i class='glyphicon glyphicon-chevron-left pull-left expand-tree'></i>
                             ".$value['name'] ."
                         <i class='glyphicon glyphicon-chevron-right pull-right expand-tree'></i>
@@ -71,10 +40,10 @@
                 }
                 echo '</div>';
             }else if(count($children)){
-                echo "<div class='branch " . $children[0]['taxonomic_rank'] . " hidden'>";
+                echo "<div class='branch " . $children[0]['taxa_name'] . " hidden'>";
                 foreach ($children as $key => $value) {
                     echo "<div class='entry'>
-                     <span class='label' data-classification='$value->level' data-id='$value->id'>
+                     <span class='label' data-classification='$value[taxa_name]' data-id='$value[id]'>
                         <i class='glyphicon glyphicon-chevron-left pull-left expand-tree'></i>
                             ".$value['name'] ."
                         <i class='glyphicon glyphicon-chevron-right pull-right expand-tree'></i>
@@ -101,45 +70,46 @@
             return 'links_tables';
         }
 
-        public function MapDatabase(){
-            $data =  $this::select('id', 'name', 'parent_id', 'taxonomic_rank')
-                ->get()
-                ->toArray();
-            return $this->mapDecendents($data);
-        }
+        
 
-        function mapDecendents($data) {
-            $fdata = array();
-            foreach($data as $k => $v)
-            {
-                if(empty($v['parent_id'])){
-                    unset($v['parent_id']);
-                    $v['data'] = array();
-                    $v['children'] = array();
-                    $fdata[] = $v;
-                }
-                else {
-                    $this->findParentAndInsert($v, $fdata);
-                }
-
-            }
-            return $fdata;
-        }
-
-        function findParentAndInsert($idata, &$ldata) {
-
-            foreach ($ldata as $k=>$v) {
-
-                if($ldata[$k]['id'] == $idata['parent_id']) {
-                    unset($idata['parent_id']);
-                    $idata['data'] = array();
-                    $idata[$idata['name']] = array();
-                    $ldata[$k][$idata['name']][] = $idata;
-                    return;
-                }
-                else if(!empty($v['children']))
-                    $this->findParentAndInsert($idata, $ldata[$k]['children']);
-            }
-        }
+//        public function getTree($parent_id, $increment = 0){
+//            $myArray = $this->getChildren($parent_id, $increment);
+//
+//            if($increment == 30) {
+//                return;
+//            }else {
+//                if (count($myArray) == 0) {
+//                    echo '</div>';
+//                } else if (count($myArray) == 1) {
+//                    echo "<div class='branch " . $myArray[0]['taxa_name'] . " hidden'>";
+//                    foreach ($myArray as $key => $value) {
+//                        if ($value['parent_id'] != $parent_id) continue;
+//                        echo "<div class='entry sole'>
+//                                 <span class='label'>
+//                                    <i class='glyphicon glyphicon-chevron-left pull-left expand-tree'></i>
+//                                        ".$value['name'] ."
+//                                    <i class='glyphicon glyphicon-chevron-right pull-right expand-tree'></i>
+//                                 </span>";
+//                        $increment++;
+//                        $this->getTree($value['id'], $increment);
+//                    }
+//                    echo '</div></div>';
+//                } else {
+//                    echo "<div class='branch " . $myArray[0]['taxa_name'] . " hidden'>";
+//                    foreach ($myArray as $key => $value) {
+//                        if ($value['parent_id'] != $parent_id) continue;
+//                        echo "<div class='entry'>
+//                             <span class='label'>
+//                                <i class='glyphicon glyphicon-chevron-left pull-left expand-tree'></i>
+//                                    ".$value['name'] ."
+//                                <i class='glyphicon glyphicon-chevron-right pull-right expand-tree'></i>
+//                             </span>";
+//                        $increment++;
+//                        $this->getTree($value['id'], $increment);
+//                    }
+//                    echo '</div></div>';
+//                }
+//            }
+//        }
 
     }
